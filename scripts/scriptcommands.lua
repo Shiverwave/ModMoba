76f94f4ca15f4635e1bc6290e82eba7096927511
Braith <apollo5601@gmail.com>
2018-07-25 10:52:05 +1000

    removed unnecessary stusff
    removed staples stuff

--- scripts/scriptcommands.lua
+++ scripts/scriptcommands.lua
@@ -1,9 +1,2 @@
-
 -- The following require has been automatically added for the module manager.
 require 'default:scriptcommands'
-
--- The following require has been automatically added for the module manager.
-require 'sceneCreator.NS_scriptcommands'
-
--- The following require has been automatically added for the module manager.
-require 'Windows for ModMoba.NS_scriptcommands'
