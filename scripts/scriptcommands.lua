c0f06fa1b5ce780b71afbbcae2b6293d04984d45
QuantumKitten <dbaynes1@goodshepherd.catholic.edu.au>
2018-07-25 10:36:36 +1000

    Hero and fixes

--- scripts/scriptcommands.lua
+++ scripts/scriptcommands.lua
@@ -1,6 +1,27 @@
+-- modded scriptcommands.lua
 
--- The following require has been automatically added for the module manager.
-require 'default:scriptcommands'
+-- Uncomment this if not using the module manager.
+--require 'default:scriptcommands'
 
--- The following require has been automatically added for the module manager.
-require 'sceneCreator.NS_scriptcommands'
+--[[
+This file will add more than one mod to the game but will allow for each
+modification to be added separately.
+the format for adding enhancements is
+
+require 'yourFolderName.yourFileName'
+
+This method is only used for when you are overriding an existing file to
+allow multiple people to edit the same file. All your other files need to
+remain in the GSCC/scripts folder with your initials in capitals as at the 
+beginning. example: NS_hellportal_effect.lua
+
+]]--
+
+
+-- Mr Staples
+
+require 'sceneCreator.createScene' -- Starts the scene editor
+require 'sceneCreator.rotateObject' -- Commands to rotate objects in game.
+
+
+
