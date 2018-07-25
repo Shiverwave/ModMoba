ca36604f9108a43478541bf48cf7c3900cd19cc5
Braith <apollo5601@gmail.com>
2018-07-25 10:49:52 +1000

    Readding stuff
    really don't know what I'm touching at this point

--- scripts/scriptcommands.lua
+++ scripts/scriptcommands.lua
@@ -1,27 +1,9 @@
--- modded scriptcommands.lua
 
--- Uncomment this if not using the module manager.
---require 'default:scriptcommands'
+-- The following require has been automatically added for the module manager.
+require 'default:scriptcommands'
 
---[[
-This file will add more than one mod to the game but will allow for each
-modification to be added separately.
-the format for adding enhancements is
+-- The following require has been automatically added for the module manager.
+require 'sceneCreator.NS_scriptcommands'
 
-require 'yourFolderName.yourFileName'
-
-This method is only used for when you are overriding an existing file to
-allow multiple people to edit the same file. All your other files need to
-remain in the GSCC/scripts folder with your initials in capitals as at the 
-beginning. example: NS_hellportal_effect.lua
-
-]]--
-
-
--- Mr Staples
-
-require 'sceneCreator.createScene' -- Starts the scene editor
-require 'sceneCreator.rotateObject' -- Commands to rotate objects in game.
-
-
-
+-- The following require has been automatically added for the module manager.
+require 'Windows for ModMoba.NS_scriptcommands'
