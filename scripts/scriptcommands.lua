4978339e07a8892a8f59f74627644bd4aa61151c
Braith <apollo5601@gmail.com>
2018-08-13 14:55:30 +1000

    still broken as flip

--- scripts/scriptcommands.lua
+++ scripts/scriptcommands.lua
@@ -1,4 +1,6 @@
+
 -- The following require has been automatically added for the module manager.
 require 'default:scriptcommands'
--- The following require hasn't been automatically added for the module manager, but by me, big boy Braith.
+
+-- The following require has been automatically added for the module manager.
 require 'BA_DynamicWindows.BA_scriptcommands'
