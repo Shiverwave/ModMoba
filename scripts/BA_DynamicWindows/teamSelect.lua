9c0dcf559b469db29bee60f48549597802ef9812
Braith <apollo5601@gmail.com>
2018-08-17 10:41:52 +1000

    Windows are easy af

--- scripts/BA_DynamicWindows/teamSelect.lua
+++ scripts/BA_DynamicWindows/teamSelect.lua
@@ -1,24 +1,20 @@
 DefaultCommandFuncs= {
-	selectTeam = function(dontUse)
+	selectTeam = function()
 	this:SystemMessage("Command beginning to run")
-		if (dontUse ~= nil) then
-			local TeamSelectionWindow = DynamicWindow(
-			"teamSelection", --(string) Window ID used to uniquely identify the window. It is returned in the DynamicWindowResponse event.
-			"Team Selection", --(string) Title of the window for the client UI
-			450, --(number) Width of the window
-			200, --(number) Height of the window
-			600, --(number) Starting X position of the window (chosen by client if not specified)
-			600 --(number) Starting Y position of the window (chosen by client if not specified)
-			)
-			TeamSelectionWindow:AddImage(40,12,"TitleBackground",250,0,"Sliced")
-			TeamSelectionWindow:AddButton(30,16,"CatLeft:","",0,0,"","",false,"LeftArrow")
-			TeamSelectionWindow:AddButton(280,16,"CatRight:","",0,0,"","",false,"RightArrow")
-			TeamSelectionWindow:AddButton(310, 8, "SelectCat:", "Back", 100, 0, "", "", false, "")
-		else
-			Usage("selectTeam")
-			return
- 		end
-		this:SystemMessage("Command Successfully Run")
+	local TeamSelectionWindow = DynamicWindow("teamSelection", "Team Selection", 450, 200)
+	TeamSelectionWindow:AddButton(2,20,"Team1","Garnet",200,120,"Join Team Garnet","",true)
+	TeamSelectionWindow:AddButton(227,20,"Team2","Emerald",200,120,"Join Team Emerald","",true)
+	this:SystemMessage("Command Successfully Run")
+	this:OpenDynamicWindow(TeamSelectionWindow)
+	end,
+	
+	shop = function()
+	this:SystemMessage("Command beginning to run")
+	local ShopWindow = DynamicWindow("shopSelection", "Shop", 200, 500)
+	ShopWindow:AddButton(120,20,"purchase","Buy",50,25,"Purchase [name]","",false)
+	this:SystemMessage("Command Successfully Run")
+	this:OpenDynamicWindow(ShopWindow)
 	end,
 }
-RegisterCommand{Command="selectTeam", Category = "God Power" , AccessLevel = AccessLevel.Mortal, Func=DefaultCommandFuncs.teamSelect, Usage="nothing yet", Desc="Opens a window for the user to select their team" }
+RegisterCommand{Command="selectTeam", Category = "God Power", AccessLevel = AccessLevel.Mortal, Func=DefaultCommandFuncs.selectTeam, Usage="[No extra argument required]", Desc="Opens a window for the user to select their team" }
+RegisterCommand{Command="shop", Category = "God Power", AccessLevel = AccessLevel.Mortal, Func=DefaultCommandFuncs.shop, Usage="[No extra argument required]", Desc="Opens a test window" }
