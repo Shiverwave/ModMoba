b0cd8ec9a31f010fa7198ecdeb9bd5fc7f3063aa
Braith <apollo5601@gmail.com>
2018-08-22 10:23:14 +1000

    Variables bruh
    Script now takes the players name and gives messages in the console as to what team they've joined. But it doesn't actually move them yet.

--- scripts/BA_DynamicWindows/teamSelect.lua
+++ scripts/BA_DynamicWindows/teamSelect.lua
@@ -2,8 +2,8 @@ DefaultCommandFuncs= {
 	selectTeam = function()
 	this:SystemMessage("Command beginning to run")
 	local TeamSelectionWindow = DynamicWindow("teamSelection", "Team Selection", 450, 200)
-	TeamSelectionWindow:AddButton(2,20,"Team1","Garnet",200,120,"Join Team Garnet","",true)
-	TeamSelectionWindow:AddButton(227,20,"Team2","Emerald",200,120,"Join Team Emerald","",true)
+	TeamSelectionWindow:AddButton(6,20,"Team1","Garnet",200,120,"Join Team Garnet","",true)
+	TeamSelectionWindow:AddButton(223,20,"Team2","Emerald",200,120,"Join Team Emerald","",true)
 	this:SystemMessage("Command Successfully Run")
 	this:OpenDynamicWindow(TeamSelectionWindow)
 	end,
@@ -16,5 +16,19 @@ DefaultCommandFuncs= {
 	this:OpenDynamicWindow(ShopWindow)
 	end,
 }
+
+RegisterEventHandler(EventType.DynamicWindowResponse,"teamSelection",
+	function (user,returnId)
+		if(returnId ~= nil) then
+			action = returnId
+			if(action == "Team1") then
+				this:SystemMessage("Adding "..this:GetName().." to Team Garnet...")
+			
+			elseif(action == 'Team2') then
+				this:SystemMessage("Adding "..this:GetName().." to Team Emerald...")
+		end
+	end
+end)
+
 RegisterCommand{Command="selectTeam", Category = "God Power", AccessLevel = AccessLevel.Mortal, Func=DefaultCommandFuncs.selectTeam, Usage="[No extra argument required]", Desc="Opens a window for the user to select their team" }
 RegisterCommand{Command="shop", Category = "God Power", AccessLevel = AccessLevel.Mortal, Func=DefaultCommandFuncs.shop, Usage="[No extra argument required]", Desc="Opens a test window" }
